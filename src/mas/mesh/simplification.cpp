59db4ff0e54d41aa07b95fad2731d848fd56887f
Antonio Sanchez <antonios@ece.ubc.ca>
2014-12-31 18:03:01 -0800

    edge collapse doesn't crash anymore, but still doesn't reproduce vcg's version

--- src/mas/mesh/simplification.cpp
+++ src/mas/mesh/simplification.cpp
@@ -4,6 +4,92 @@
 namespace mas {
 namespace mesh {
 
+void VERIFY_HE_INCIDENCE(PolygonMesh& mesh) {
+
+    // number of faces each vertex is part of
+    std::vector<int> vtxFaceCount(mesh.numVertices(), 0);
+
+    for (SharedPolygon& face : mesh.faces) {
+        for (SharedVertex3d& vtx : face->verts) {
+            vtxFaceCount[vtx->idx]++;
+        }
+    }
+
+    for (SharedVertex3d& vtx : mesh.verts) {
+        // verify count
+        if (vtx->incident.size() != vtxFaceCount[vtx->idx]) {
+            throw mas::exception("incorrect number of incident edges");
+        }
+
+        for (HalfEdge* he : vtx->incident) {
+            if (he->head != vtx) {
+                throw mas::exception("half-edge not incident to head vertex!");
+            }
+        }
+    }
+
+}
+
+void VERIFY_CONNECTIVITY(PolygonMesh& mesh) {
+
+    for (SharedPolygon& face : mesh.faces) {
+        HalfEdge* he0 = face->he0.get();
+        HalfEdge* he = he0;
+
+        do {
+            if (he->opposite == nullptr) {
+                throw mas::exception("mesh is open");
+            } else if (he->getIndex() != he->opposite->getIndex()) {
+                throw mas::exception("half-edges incorrectly numbered");
+            }
+            he = he->next.get();
+        } while (he != he0);
+    }
+
+    for (SharedVertex3d& vtx : mesh.verts) {
+        for (HalfEdge* edge : vtx->getIncidentEdges()) {
+            if (edge->face != mesh.faces[edge->face->idx].get()) {
+                throw mas::exception("incident edges broken");
+            }
+            bool found = false;
+            for (SharedVertex3d& vtx2 : edge->face->verts) {
+                if (vtx2 == vtx) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                throw mas::exception("face doesn't contain vertex");
+            }
+        }
+    }
+}
+
+void VERIFY_VERTEX_REMOVED(PolygonMesh& mesh, SharedVertex3d& rvtx) {
+
+    // check indexing
+    size_t idx = 0;
+    for (SharedVertex3d& vtx : mesh.verts) {
+        if (vtx == rvtx) {
+            throw mas::exception("Vertex still belongs to mesh");
+        }
+        if (vtx->getIndex() != idx) {
+            throw mas::exception("Vertex incorrectly indexed");
+        }
+        idx++;
+    }
+
+    // check that no face still contains the vertex
+    for (SharedPolygon& face : mesh.faces) {
+        for (SharedVertex3d& vtx : face->verts) {
+            if (vtx == rvtx) {
+                throw mas::exception("Vertex still belongs to a face");
+            }
+        }
+    }
+
+}
+
 EdgeCollapseQuadric::EdgeCollapseQuadric() :
         A(), b(), c() {
     A.setZero();
@@ -68,27 +154,34 @@ void EdgeCollapseQuadric::solve(const Point3d& v1, const Point3d& v2,
         v.z = m02 * b.x + m12 * b.y + m22 * b.z;
 
     } else {
-        // constrain to line connecting vertices
-        // (v2-v1)^TA(v2-v1) * t = - [v1^TA(v2-v1)+b^T(v2-v1)], t in [0, 1]
-        v.subtract(v2, v1);
-        double Adx = A[IDX3D_00] * v.x + A[IDX3D_01] * v.y + A[IDX3D_02] * v.z;
-        double Ady = A[IDX3D_01] * v.x + A[IDX3D_11] * v.y + A[IDX3D_12] * v.z;
-        double Adz = A[IDX3D_02] * v.x + A[IDX3D_12] * v.y + A[IDX3D_22] * v.z;
-
-        double rhs = -b.dot(v) - v1.dot(Adx, Ady, Adz);
-        double lhs = v.dot(Adx, Ady, Adz);
-
-        double t = 0.5;
-        if (lhs != 0) {
-            double t = rhs / lhs;
-            if (t < 0) {
-                t = 0;
-            } else if (t > 1) {
-                t = 0;
-            }
-        }
-        v.interpolate(v1, t, v2);
+        solveConstrained(v1, v2, v);
     }
+}
+
+void EdgeCollapseQuadric::solveConstrained(const Point3d& v1, const Point3d& v2,
+        Point3d &v) {
+
+    // constrain to line connecting vertices
+    // (v2-v1)^TA(v2-v1) * t = - [v1^TA(v2-v1)+b^T(v2-v1)], t in [0, 1]
+    v.subtract(v2, v1);
+    double Adx = A[IDX3D_00] * v.x + A[IDX3D_01] * v.y + A[IDX3D_02] * v.z;
+    double Ady = A[IDX3D_01] * v.x + A[IDX3D_11] * v.y + A[IDX3D_12] * v.z;
+    double Adz = A[IDX3D_02] * v.x + A[IDX3D_12] * v.y + A[IDX3D_22] * v.z;
+
+    double rhs = -b.dot(v) - v1.dot(Adx, Ady, Adz);
+    double lhs = v.dot(Adx, Ady, Adz);
+
+    double t = 0.5;
+    if (lhs != 0) {
+        double t = rhs / lhs;
+        if (t < 0) {
+            t = 0;
+        } else if (t > 1) {
+            t = 0;
+        }
+    }
+    v.interpolate(v1, t, v2);
+
 }
 
 double EdgeCollapseQuadric::cost(const Point3d &v) {
@@ -135,10 +228,10 @@ void EdgeCollapseQuadricCost::init(PolygonMesh& mesh) {
             Qi.add(Kp[fidx]);
         }
 
-        //        std::cout << "Qv[" << i << "]: " << Qi.A(0,0) << " " << Qi.A(0,1) << " " << Qi.A(0,2) << " " << Qi.b[0] <<  std::endl;
-        //        std::cout << "       " << Qi.A(1,0) << " " << Qi.A(1,1) << " " << Qi.A(1,2) << " " << Qi.b[1] <<  std::endl;
-        //        std::cout << "       " << Qi.A(2,0) << " " << Qi.A(2,1) << " " << Qi.A(2,2) << " " << Qi.b[2] <<  std::endl;
-        //        std::cout << "       " << Qi.b[0] << " " << Qi.b[1] << " " << Qi.b[2] << " " << Qi.c <<  std::endl;
+        std::cout << "Qv[" << i << "]: " << Qi.A(0,0) << " " << Qi.A(0,1) << " " << Qi.A(0,2) << " " << Qi.b[0] <<  std::endl;
+        std::cout << "       " << Qi.A(1,0) << " " << Qi.A(1,1) << " " << Qi.A(1,2) << " " << Qi.b[1] <<  std::endl;
+        std::cout << "       " << Qi.A(2,0) << " " << Qi.A(2,1) << " " << Qi.A(2,2) << " " << Qi.b[2] <<  std::endl;
+        std::cout << "       " << Qi.b[0] << " " << Qi.b[1] << " " << Qi.b[2] << " " << Qi.c <<  std::endl;
 
         Qv.push_back(std::move(Qi));
     }
@@ -155,15 +248,27 @@ double EdgeCollapseQuadricCost::operator ()(PolygonMesh& mesh, HalfEdge &he,
     EdgeCollapseQuadric ecq(Qv[he.head->getIndex()]);
     ecq.add(Qv[he.tail->getIndex()]);
     ecq.solve(*(he.head), *(he.tail), v);
+
+    // don't let it move too far
+    double dmax = he.head->distanceSquared(v);
+    double d = he.tail->distanceSquared(v);
+    if (d > dmax) {
+        dmax = d;
+    }
+    double edgelen = he.tail->distanceSquared(*(he.head));
+    if (dmax > edgelen) {
+        ecq.solveConstrained(*(he.head), *(he.tail), v);
+    }
+
     double c = ecq.cost(v);
 
-    //    std::cout << "Edge " << he.idx << ": " << he.head->idx << " --> " << he.tail->idx << std::endl;
-    //    std::cout << "    Q: " << ecq.A(0,0) << " " << ecq.A(0,1) << " " << ecq.A(0,2) << " " << ecq.b[0] <<  std::endl;
-    //    std::cout << "       " << ecq.A(1,0) << " " << ecq.A(1,1) << " " << ecq.A(1,2) << " " << ecq.b[1] <<  std::endl;
-    //    std::cout << "       " << ecq.A(2,0) << " " << ecq.A(2,1) << " " << ecq.A(2,2) << " " << ecq.b[2] <<  std::endl;
-    //    std::cout << "       " << ecq.b[0] << " " << ecq.b[1] << " " << ecq.b[2] << " " << ecq.c <<  std::endl;
-    //    std::cout << "    v: " << v.x << " " << v.y << " " << v.z << std::endl;
-    //    std::cout << "    c: " << c << std::endl << std::endl;
+    std::cout << "Edge " << he.idx << ": " << he.head->idx << " --> " << he.tail->idx << std::endl;
+    std::cout << "    Q: " << ecq.A(0,0) << " " << ecq.A(0,1) << " " << ecq.A(0,2) << " " << ecq.b[0] <<  std::endl;
+    std::cout << "       " << ecq.A(1,0) << " " << ecq.A(1,1) << " " << ecq.A(1,2) << " " << ecq.b[1] <<  std::endl;
+    std::cout << "       " << ecq.A(2,0) << " " << ecq.A(2,1) << " " << ecq.A(2,2) << " " << ecq.b[2] <<  std::endl;
+    std::cout << "       " << ecq.b[0] << " " << ecq.b[1] << " " << ecq.b[2] << " " << ecq.c <<  std::endl;
+    std::cout << "    v: " << v.x << " " << v.y << " " << v.z << std::endl;
+    std::cout << "    c: " << c << std::endl << std::endl;
 
     return c;
 }
@@ -179,7 +284,11 @@ void EdgeCollapseQuadricCost::operator ()(HalfEdge &he) {
     size_t hidx = he.head->getIndex();
     size_t tidx = he.tail->getIndex();
     size_t lastidx = Qv.size()-1;
+
+    // update cost for vertex at the head of the edge
     Qv[hidx].add(Qv[tidx]);
+
+    // vertex indices may shuffle with replacement
     Qv[tidx] = std::move(Qv[lastidx]);
     Qv.pop_back();  // remove
 }
--- test/mas/mesh/simplification_test.cpp
+++ test/mas/mesh/simplification_test.cpp
@@ -8,19 +8,21 @@
 #include "mas/core/time.h"
 #include "mas/mesh/simplification.h"
 #include "mas/core/exception.h"
+#include <algorithm>
 
 void printstuff(mas::mesh::PolygonMesh& mesh, int offset) {
 
     // vertices
     for (mas::mesh::SharedVertex3d& vtx : mesh.verts) {
-        std::cout << "v(" << (vtx->idx+offset) << ",:) = [" << vtx->toString("%g, ", "%g") << "];"<< std::endl;
+        std::cout << "v(" << (vtx->idx + offset) << ",:) = ["
+                << vtx->toString("%g, ", "%g") << "];" << std::endl;
     }
 
     // faces
     for (mas::mesh::SharedPolygon& face : mesh.faces) {
-        std::cout << "f(" << (face->idx+offset) << ",:) = [";
-        for (int i=0; i<face->numVertices()-1; i++) {
-            std::cout << (face->verts[i]->idx+offset) << ", ";
+        std::cout << "f(" << (face->idx + offset) << ",:) = [";
+        for (int i = 0; i < face->numVertices() - 1; i++) {
+            std::cout << (face->verts[i]->idx + offset) << ", ";
         }
         std::cout << (face->verts.back()->idx + offset) << "];" << std::endl;
     }
@@ -32,7 +34,9 @@ void printstuff(mas::mesh::PolygonMesh& mesh, int offset) {
         mas::mesh::HalfEdge* he = he0;
         do {
             if (he->isPrimary()) {
-                std::cout << "e(" << (he->idx+offset) << ",:) = [" << (he->tail->idx + offset) << ", " <<  (he->head->idx + offset) << "];"<< std::endl;
+                std::cout << "e(" << (he->idx + offset) << ",:) = ["
+                        << (he->tail->idx + offset) << ", "
+                        << (he->head->idx + offset) << "];" << std::endl;
             }
             he = he->next.get();
         } while (he != he0);
@@ -40,68 +44,125 @@ void printstuff(mas::mesh::PolygonMesh& mesh, int offset) {
 
 }
 
-bool doEdgeCollapseTest(const char filename[], const char out[], int reduce) {
+bool doMallocDebug(const char filename[]) {
+    mas::mesh::io::SimpleObjReader reader;
+    mas::mesh::io::SimpleObjWriter writer;
 
-   mas::mesh::io::SimpleObjReader reader;
-   mas::mesh::io::SimpleObjWriter writer;
+    using mas::mesh::PolygonMesh;
+    using mas::Point3d;
+    using mas::mesh::SharedPolygon;
+    using mas::mesh::SharedHalfEdge;
+    using mas::mesh::HalfEdge;
 
-   using mas::mesh::PolygonMesh;
-   using mas::Point3d;
-   using mas::mesh::SharedPolygon;
-   using mas::mesh::SharedHalfEdge;
-   using mas::mesh::HalfEdge;
+    std::vector<SharedPolygon> facesCopy;
+    // local scope
+    {
+        std::unique_ptr<PolygonMesh> mesh = std::unique_ptr<PolygonMesh>(
+                reader.read(filename));
 
-   std::unique_ptr<PolygonMesh> mesh = std::unique_ptr<PolygonMesh>(reader.read(filename));
-   if (mesh == nullptr) {
-       std::cout << "Cannot find mesh " << filename << std::endl;
-       return false;
-   }
-   mesh->connect(); // build connectivity
+        if (mesh == nullptr) {
+            std::cout << "Cannot find mesh " << filename << std::endl;
+            return false;
+        }
 
-   if (mesh->numFaces() < 20) {
-       printstuff(*mesh, 0);
-   }
+        mesh->connect(); // build connectivity
 
-   std::cout << "# faces: "<< mesh->numFaces() << std::endl;
-   std::cout << "# vertices: "<< mesh->numVertices() << std::endl;
+        facesCopy = mesh->faces;
+    }
 
-   // check connectivity
-   for (SharedPolygon& face : mesh->faces) {
-       HalfEdge* he0 = face->he0.get();
-       HalfEdge* he = he0;
-       do {
-           if (he->opposite == nullptr) {
-               std::cout << "OPPOSITE BROKEN!!!" << std::endl;
-           }
-           if (he->face != face.get()) {
-               std::cout << "FACE BROKEN!!!" << std::endl;
-           }
-           he = he->next.get();
-       } while (he != he0);
-   }
+    // dealloc faces one at a time
+    size_t len = facesCopy.size();
+    std::reverse(facesCopy.begin(), facesCopy.end());
+    for (int i = 0; i < len; i++) {
+        size_t idx = facesCopy.back()->idx;
+        facesCopy.pop_back();
+        facesCopy.shrink_to_fit();
+        std::cout << "deallocated face " << idx << std::endl;
+    }
 
-   mas::mesh::edge_collapse(*mesh, mesh->numFaces()-reduce);
-
-   writer.write(*mesh, out);
-
-   return true;
+    return true;
 }
 
-int main( int argc, const char* argv[] ) {
+bool doEdgeCollapseTest(const char filename[], const char out[], int reduce) {
 
-   int reduce = 1;
-   if (argc > 3) {
-       sscanf(argv[3], "%d", &reduce);
-   }
+    mas::mesh::io::SimpleObjReader reader;
+    mas::mesh::io::SimpleObjWriter writer;
 
-   mas::time::Timer timer;
-   timer.start();
-   doEdgeCollapseTest(argv[1], argv[2], reduce);
-   timer.stop();
-   double micros = timer.getMicroseconds();
-   printf("Took %f us\n", micros);
+    using mas::mesh::PolygonMesh;
+    using mas::Point3d;
+    using mas::mesh::SharedPolygon;
+    using mas::mesh::SharedHalfEdge;
+    using mas::mesh::HalfEdge;
 
+    std::unique_ptr<PolygonMesh> mesh = std::unique_ptr<PolygonMesh>(
+            reader.read(filename));
 
-   return 0;
+    if (mesh == nullptr) {
+        std::cout << "Cannot find mesh " << filename << std::endl;
+        return false;
+    }
+
+    mesh->connect(); // build connectivity
+
+    if (mesh->numFaces() < 20) {
+        printstuff(*mesh, 0);
+    }
+
+    std::cout << "# faces: " << mesh->numFaces() << std::endl;
+    std::cout << "# vertices: " << mesh->numVertices() << std::endl;
+
+    // check connectivity
+    for (SharedPolygon& face : mesh->faces) {
+        HalfEdge* he0 = face->he0.get();
+        HalfEdge* he = he0;
+        do {
+            if (he->opposite == nullptr) {
+                std::cout << "OPPOSITE BROKEN!!!" << std::endl;
+            }
+            if (he->face != face.get()) {
+                std::cout << "FACE BROKEN!!!" << std::endl;
+            }
+            he = he->next.get();
+        } while (he != he0);
+    }
+
+    mas::mesh::EdgeCollapseQuadricCost cost;
+    int len = mesh->numFaces()-reduce;
+    if (len < 0) {
+        len = 2;
+    }
+    mas::mesh::edge_collapse(*mesh, (size_t)len, cost, cost);
+
+    writer.write(*mesh, out);
+
+    std::cout << "# faces: " << mesh->numFaces() << std::endl;
+    std::cout << "# vertices: " << mesh->numVertices() << std::endl;
+    if (mesh->numFaces() < 20) {
+        printstuff(*mesh, 0);
+    }
+
+    // remove faces?
+    mesh->disconnect();
+
+    return true;
+}
+
+int main(int argc, const char* argv[]) {
+
+    int reduce = 1;
+    if (argc > 3) {
+        sscanf(argv[3], "%d", &reduce);
+    }
+
+    mas::time::Timer timer;
+    timer.start();
+    doEdgeCollapseTest(argv[1], argv[2], reduce);
+    // doMallocDebug(argv[1]);
+    timer.stop();
+    double micros = timer.getMicroseconds();
+    printf("Took %f us\n", micros);
+    fflush(stdout);
+
+    return 0;
 }
 
