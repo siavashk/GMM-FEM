0cc9b8c511079cf6ffe48bbbfd2152a072f01f47
antonio <antonio@panthera.ca>
2015-01-08 10:20:50 -0800

    parallel_quick_sort shows same malloc error

--- test/mas/concurrency/parallel_sort_test.cpp
+++ test/mas/concurrency/parallel_sort_test.cpp
@@ -1,4 +1,5 @@
 #include "mas/concurrency/thread.h"
+#include "mas/core/time.h"
 #include <list>
 #include <chrono>
 #include <iostream>
@@ -7,13 +8,13 @@ template<typename T>
 struct sorter {
     mas::concurrency::thread_pool pool;
 
-    std::vector<T> do_sort(std::vector<T>& chunk_data, int& counter) {
+    std::vector<T> do_sort(std::vector<T>&& chunk_data, int& counter) {
         if (chunk_data.empty()) {
             return chunk_data;
         }
 
         std::vector<T> middle;
-        middle.push_back(chunk_data.front());
+        middle.push_back(std::move(chunk_data.front()));
         T const& partition_val = *middle.begin();
 
         std::vector<T> new_lower_chunk;
@@ -22,19 +23,19 @@ struct sorter {
         int nupper = 0;
         for (int i=1; i<chunk_data.size(); i++) {
             if (chunk_data[i] > partition_val) {
-                chunk_data[nupper++] = chunk_data[i];
+                chunk_data[nupper++] = std::move(chunk_data[i]);
             } else if (chunk_data[i] == partition_val) {
-                middle.push_back(chunk_data[i]);
+                middle.push_back(std::move(chunk_data[i]));
             } else {
-                new_lower_chunk.push_back(chunk_data[i]);
+                new_lower_chunk.push_back(std::move(chunk_data[i]));
             }
         }
         chunk_data.resize(nupper);
 
-        auto f = std::bind(&sorter::do_sort, this, std::move(new_lower_chunk), std::ref(counter));
+        auto f = mas::concurrency::bind_simple(&sorter::do_sort, this, std::move(new_lower_chunk), std::ref(counter));
         std::future<std::vector<T> > new_lower = pool.submit_back(f);
 
-        std::vector<T> new_higher(do_sort(chunk_data, counter));
+        std::vector<T> new_higher(do_sort(std::move(chunk_data), counter));
 
         while (!(new_lower.wait_for(std::chrono::seconds(0)) == std::future_status::ready)) {
             pool.run_pending_task();
@@ -43,10 +44,10 @@ struct sorter {
         std::vector<T> result = std::move(new_lower.get());
         result.reserve(result.size()+middle.size()+new_higher.size());
         for (int i=0; i<middle.size(); i++) {
-            result.push_back(middle[i]);
+            result.push_back(std::move(middle[i]));
         }
         for (int i=0; i<new_higher.size(); i++) {
-            result.push_back(new_higher[i]);
+            result.push_back(std::move(new_higher[i]));
         }
 
         return result;
@@ -54,30 +55,41 @@ struct sorter {
 };
 
 template<typename T>
-std::vector<T> parallel_quick_sort(std::vector<T> input) {
+std::vector<T> parallel_quick_sort(std::vector<T>&& input) {
     if (input.empty()) {
         return input;
     }
     sorter<T> s;
     int counter = 10;
-    return s.do_sort(input, counter);
+    return s.do_sort(std::move(input), counter);
 }
 
 int main(int argc, char **argv) {
 
     std::vector<double> vals;
-    for (int i=0; i<100; i++) {
+    int N = 1000000;
+    for (int i=0; i<N; i++) {
         vals.push_back(i);
     }
     std::random_shuffle(vals.begin(), vals.end());
 
-    std::vector<double> out = parallel_quick_sort(vals);
+    mas::time::Timer timer;
+    timer.start();
+    std::vector<double> out = parallel_quick_sort(std::move(vals));
+    timer.stop();
+    double ms = timer.getMilliseconds();
 
-    for (int i=0; i<100; i++) {
-        std::cout << out[i] << " ";
+    if (N <= 100) {
+        for (int i=0; i<N; i++) {
+            std::cout << out[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
 
+    printf("Finished in %g ms\n", ms);
+    fflush(stdout);
+
+    std::cout << "DONE" << std::endl;
 
 }
 
