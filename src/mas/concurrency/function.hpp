14c042c108e29d3a452c78934ed0fddc35817747
Antonio Sanchez <antonios@ece.ubc.ca>
2015-01-11 11:26:16 -0800

    having trouble with wrapping now

--- src/mas/concurrency/function.hpp
+++ src/mas/concurrency/function.hpp
@@ -18,7 +18,7 @@ struct target_type<Member Class::*> {
 	typedef Class type;
 };
 
-//Is reference to pointer target or derived
+//is reference to pointer target or derived
 template<typename Object, typename Pointer>
 struct is_target_reference: public std::integral_constant<bool,
 		std::is_reference<Object>::value
@@ -36,6 +36,7 @@ inline typename std::enable_if<
 
 		typename std::result_of<Functor(Object,Args&&...)>::type>::type invoke(
 		Functor&& functor, Object&& object, Args&&... args) {
+	std::cout << "MEMBER FUNCTION, BY REFERENCE" << std::endl;
 	return (object.*functor)(std::forward<Args>(args)...);
 }
 
@@ -48,6 +49,7 @@ inline typename std::enable_if<
 
 		typename std::result_of<Functor(Object,Args&&...)>::type>::type invoke(
 		Functor&& functor, Object&& object,	Args&&... args) {
+	std::cout << "MEMBER FUNCTION, BY POINTER" << std::endl;
 	return ((*std::forward<Object>(object)).*functor)(	std::forward<Args>(args)...);
 }
 
@@ -59,6 +61,7 @@ inline typename std::enable_if<
 				typename std::decay<Functor>::type>::value,
 		typename std::result_of<Functor(Object)>::type>::type
 		invoke(Functor&& functor, Object&& object) {
+	std::cout << "MEMBER OBJECT, BY REFERENCE" << std::endl;
 	return object.*functor;
 }
 
@@ -70,6 +73,7 @@ inline typename std::enable_if<
 				typename std::decay<Functor>::type>::value,
 		typename std::result_of<Functor(Object)>::type>::type
 		invoke(Functor&& functor, Object&& object) {
+	std::cout << "MEMBER OBJECT, BY POINTER" << std::endl;
 	return (*std::forward<Object>(object)).*functor;
 }
 
@@ -87,7 +91,7 @@ inline typename std::enable_if<
 template<typename Functor, typename ... Args>
 inline typename std::enable_if<
 		!std::is_member_pointer<typename std::decay<Functor>::type>::value,
-		typename std::result_of<Functor&(Args...)>::type>::type
+		typename std::result_of<Functor&(Args&&...)>::type>::type
 		invoke(Functor&& functor, Args&&... args) {
 	std::cout << "REGULAR FUNCTION" << std::endl;
 	return std::forward<Functor>(functor)(std::forward<Args>(args)...);
@@ -119,7 +123,7 @@ class fun_type;
 template<typename Fn, typename ... Args>
 class function_wrapper {
 public:
-	typedef typename std::result_of<Fn(Args...)>::type result_type;
+	typedef typename std::result_of<Fn&(Args&&...)>::type result_type;
 
 private:
 	std::tuple<Fn, Args&&...> bound;
@@ -158,16 +162,6 @@ public:
 };
 
 template<typename Fn, typename ... Args>
-function_wrapper<Fn, Args...> wrap(Fn&& fn, Args&&... args) {
-	return function_wrapper<Fn, Args...>(std::move(fn),	std::forward<Args>(args)...);
-}
-
-template<typename Fn, typename ... Args>
-function_wrapper<Fn, Args...> wrap(const Fn& fn, Args&&... args) {
-	return function_wrapper<Fn, Args...>(fn, std::forward(args)...);
-}
-
-template<typename Fn, typename ... Args>
 class future_function_wrapper {
 	typedef typename std::result_of<Fn(Args...)>::type result_type;
 
@@ -178,7 +172,7 @@ private:
 	template<std::size_t ... Indices>
 	void invoke(index_tuple<Indices...>) {
 		// std::bind always forwards bound arguments as lvalues,
-		// but this type can call functions which only accept rvalues.
+		// but this type can call functions which accept rvalues.
 		//return std::forward<Fn>(std::get<0>(bound))(std::forward<Args>(std::get<Indices+1>(bound))...);
 		promise.set_value(
 				mas::concurrency::invoke(std::forward<Fn>(std::get<0>(bound)),std::forward<Args>(std::get<Indices+1>(bound))...));
@@ -211,82 +205,69 @@ public:
 
 };
 
-//class future_function_wrapper {
-//	struct ffw_base {
-//		virtual void invoke() = 0;
-//	};
-//
-//	template<typename Fn, typename ... Args>
-//	struct ffw : ffw_base {
-//		typedef typename function_wrapper<Fn,Args...>::result_type result_type;
-//		function_wrapper<Fn,Args...> f;
-//		std::promise<result_type> p;
-//
-//		ffw(const Fn& fn, Args&&... args) {
-//			f = wrap(fn, std::forward<Args>(args)...);
-//		}
-//
-//		ffw(Fn&& fn, Args&&... args) {
-//			f = wrap(std::move(fn), std::forward<Args>(args)...);
-//		}
-//
-//		ffw(function_wrapper<Fn,Args...>&& fw, std::promise<result_type>&& promise) :
-//			f(std::move(fw)), p(std::move(promise)){
-//		}
-//
-//		std::future<result_type> get_future() {
-//			return p.get_future();
-//		}
-//
-//		void invoke() {
-//			p.set(f.invoke());
-//		}
-//	};
-//
-//	std::unique_ptr<ffw_base> f;
-//
-//public:
-//	template<typename Fn, typename... Args>
-//	explicit future_function_wrapper(
-//			std::future<typename function_wrapper<Fn,Args...>::result_type>& future,
-//			Fn&& fn, Args&&... args) {
-//		auto ff = new ffw<Fn,Args...>(std::move(fn), std::forward<Args>(args)...);
-//		future = std::move(ff->get_future());
-//		f = std::unique_ptr<ffw_base>(ff);
-//	}
-//
-//	template<typename Fn, typename... Args>
-//	explicit future_function_wrapper(
-//			std::future<typename function_wrapper<Fn,Args...>::result_type>& future,
-//			const Fn& fn, Args&&... args) {
-//		auto ff = new ffw<Fn,Args...>(fn, std::forward<Args>(args)...);
-//		future = std::move(ff->get_future());
-//		f = std::unique_ptr<ffw_base>(ff);
-//	}
-//
-//	template<typename Fn, typename... Args>
-//		explicit future_function_wrapper(
-//				function_wrapper<Fn,Args...>&& fw,
-//				std::promise<typename function_wrapper<Fn,Args...>::result_type>&& p) {
-//		f = std::unique_ptr<ffw_base>(new ffw<Fn,Args...>(std::move(fw), std::move(p)));
-//	}
-//
-//	void invoke() {
-//		f->invoke();
-//	}
-//
-//};
+template<typename Tp>
+struct maybe_wrap_pointer {
+    typedef Tp type;
 
+    static const Tp&  __do_wrap(const Tp& x) {
+        return x;
+    }
+
+    static Tp&& __do_wrap(Tp&& x) {
+        return static_cast<Tp&&>(x);
+    }
+};
+
+template<>
+struct maybe_wrap_pointer<void> {
+    typedef void type;
+};
+
+template<typename Fn, typename... Args>
+struct wrap_helper {
+    typedef maybe_wrap_pointer<typename std::decay<Fn>::type> maybe_type;
+    typedef typename maybe_type::type fn_type;
+    typedef function_wrapper<fn_type(typename std::decay<Args>::type...)> type;
+};
+
+
+//// Simplified version of std::bind for internal use, without support for
+//// unbound arguments, placeholders or nested bind expressions.
+//template<typename _Callable, typename ... _Args>
+//typename detail::_Bind_simple_helper<_Callable, _Args...>::__type bind_simple(
+//        _Callable&& __callable, _Args&&... __args) {
+//    typedef detail::_Bind_simple_helper<_Callable, _Args...> __helper_type;
+//    typedef typename __helper_type::__maybe_type __maybe_type;
+//    typedef typename __helper_type::__type __result_type;
+//    return __result_type(
+//            __maybe_type::__do_wrap(std::forward<_Callable>(__callable)),
+//            std::forward<_Args>(__args)...);
+//}
+
+template<typename Fn, typename ... Args>
+typename wrap_helper<Fn,Args...>::type wrap(Fn&& fn, Args&&... args) {
+	typedef wrap_helper<Fn, Args...> helper_type;
+	typedef typename helper_type::maybe_type maybe_type;
+	typedef typename helper_type::type result_type;
+	return result_type(
+			maybe_type::__do_wrap(std::forward<Fn>(fn),std::forward<Args>(args)...));
+	//return function_wrapper<Fn, Args...>(std::forward<Fn>(fn),std::forward<Args>(args)...);
+}
+
+//template<typename Fn, typename ... Args>
+//function_wrapper<Fn, Args...> wrap(const Fn& fn, Args&&... args) {
+//	return function_wrapper<Fn, Args...>(fn, std::forward<Args>(args)...);
+//}
 
 template<typename Fn, typename ... Args>
 future_function_wrapper<Fn, Args...> wrap_future(Fn&& fn, Args&&... args) {
 	return future_function_wrapper<Fn, Args...>(std::move(fn), std::forward<Args>(args)...);
 }
 
-template<typename Fn, typename ... Args>
-future_function_wrapper<Fn, Args...> wrap_future(const Fn& fn, Args&&... args) {
-	return future_function_wrapper<Fn, Args...>(fn, std::forward(args)...);
-}
+//template<typename Fn, typename ... Args>
+//future_function_wrapper<Fn, Args...> wrap_future(const Fn& fn, Args&&... args) {
+//	return future_function_wrapper<Fn, Args...>(fn, std::forward<Args>(args)...);
+//}
 
 }
 }
--- test/mas/concurrency/tuple_test.cpp
+++ test/mas/concurrency/tuple_test.cpp
@@ -1,289 +1,138 @@
 #include <iostream>
 #include <memory>
-#include <utility>
-#include <tuple>
-#include <thread>
-#include <functional>
 #include <string>
 #include <vector>
+#include "mas/concurrency/bind_simple.hpp"
+#include "mas/concurrency/function.hpp"
 
-namespace type_traits {
-template<typename T>
-struct target_type {
-	typedef void type;
-};
-
-template<typename Class, typename Member>
-struct target_type<Member Class::*> {
-	typedef Class type;
-};
-
-//Is reference to pointer target or derived
-template<typename Object, typename Pointer>
-struct is_target_reference: public std::integral_constant<bool,
-		std::is_reference<Object>::value
-				&& std::is_base_of<typename target_type<Pointer>::type,
-						typename std::decay<Object>::type>::value> {
-};
-}
-
-template<typename Functor, typename Object, typename ... Args>
-inline typename std::enable_if<
-		std::is_member_function_pointer<typename std::decay<Functor>::type>::value
-				&& type_traits::is_target_reference<Object&&,
-						typename std::decay<Functor>::type>::value,
-
-		typename std::result_of<Functor(Object,Args&&...)>::type>::type invoke(
-		Functor&& functor, Object&& object, Args&&... args) {
-	std::cout << "MEMBER FUNCTION, REFERENCE" << std::endl;
-	return (object.*functor)(std::forward<Args>(args)...);
-}
-
-template<typename Functor, typename Object, typename ... Args>
-inline typename std::enable_if<
-		std::is_member_function_pointer<typename std::decay<Functor>::type>::value
-			&& !type_traits::is_target_reference<Object&&,
-				typename std::decay<Functor>::type>::value,
-
-		typename std::result_of<Functor(Object,Args&&...)>::type>::type invoke(
-		Functor&& functor, Object&& object,	Args&&... args) {
-	std::cout << "MEMBER FUNCTION, POINTER" << std::endl;
-	return ((*std::forward<Object>(object)).*functor)(	std::forward<Args>(args)...);
-}
-
-template<typename Functor, typename Object>
-inline typename std::enable_if<
-		std::is_member_object_pointer<typename std::decay<Functor>::type>::value
-			&& type_traits::is_target_reference<Object&&,
-				typename std::decay<Functor>::type>::value,
-		typename std::result_of<Functor(Object)>::type>::type
-		invoke(Functor&& functor, Object&& object) {
-	std::cout << "MEMBER OBJECT, REFERENCE" << std::endl;
-	return object.*functor;
-}
-
-template<typename Functor, typename Object>
-inline typename std::enable_if<
-		std::is_member_object_pointer<typename std::decay<Functor>::type>::value
-			&& !type_traits::is_target_reference<Object&&,
-				typename std::decay<Functor>::type>::value,
-		typename std::result_of<Functor(Object)>::type>::type
-		invoke(Functor&& functor, Object&& object) {
-	std::cout << "MEMBER OBJECT, POINTER" << std::endl;
-	return (*std::forward<Object>(object)).*functor;
-}
-
-template<typename Functor, typename ... Args>
-inline typename std::enable_if<
-		std::is_member_pointer<typename std::decay<Functor>::type>::value,
-		typename std::result_of<Functor(Args...)>::type>::type
-		invoke(Functor&& functor, Args&&... args) {
-	std::cout << "WHAT IS THIS?" << std::endl;
-	return std::forward<Functor>(functor)(std::forward<Args>(args)...);
-}
-
-template<typename Functor, typename ... Args>
-inline typename std::enable_if<
-		!std::is_member_pointer<typename std::decay<Functor>::type>::value,
-		typename std::result_of<Functor&(Args...)>::type>::type
-		invoke(Functor&& functor, Args&&... args) {
-	std::cout << "REGULAR FUNCTION" << std::endl;
-	return std::forward<Functor>(functor)(std::forward<Args>(args)...);
-}
-
-///**
-// * Invoke a function object, which may be either a member pointer or a
-// * function object. The first parameter will tell which.
-// */
-//template<typename _Functor, typename ... _Args>
-//inline typename std::enable_if<
-//(		!std::is_member_pointer<_Functor>::value
-//		&& !std::is_function<_Functor>::value
-//		&& !std::is_function<typename std::remove_pointer<_Functor>::type>::value
-//		&& !std::is_function<typename std::remove_reference<_Functor>::type>::value),
-//		typename std::result_of<_Functor&(_Args&&...)>::type>::type __invoke_(
-//				_Functor&& __f, _Args&&... __args) {
-//	std::cout << "Operator" << std::endl;
-//	return __f(std::forward<_Args>(__args)...);
-//}
-//
-//template<typename _Functor, typename ... _Args>
-//inline typename std::enable_if<
-//(std::is_member_pointer<_Functor>::value
-//		&& !std::is_function<_Functor>::value
-//		&& !std::is_function<
-//		typename std::remove_pointer<_Functor>::type>::value),
-//		typename std::result_of<_Functor(_Args&&...)>::type>::type __invoke_(
-//				_Functor& __f, _Args&&... __args) {
-//	std::cout << "Member operator?" << std::endl;
-//	return std::mem_fn(__f)(std::forward<_Args>(__args)...);
-//}
-//
-//template<typename _Functor, typename ... _Args>
-//inline typename std::enable_if<
-//(std::is_member_function_pointer<_Functor>::value),
-//typename std::result_of<_Functor(_Args&&...)>::type>::type __invoke_(
-//		_Functor&& __f, _Args&&... __args) {
-//	std::cout << "Member function" << std::endl;
-//	return std::mem_fn(__f)(std::forward<_Args>(__args)...);
-//}
-//
-	template<typename Functor, typename Object, typename ... Args>
-	void __test_(Functor&& __f, Object&& object,Args&&... __args) {
-		std::cout << "Member function: " << std::is_member_function_pointer<Functor>::value << std::endl;
-		std::cout << "Decayed function pointer: " << std::is_member_function_pointer<typename std::decay<Functor>::type>::value << std::endl;
-		std::cout << "Target reference: " << type_traits::is_target_reference<Object&&, typename std::decay<Functor>::type>::value << std::endl;
+int regular_function(std::vector<int>&& i) {
+	int sum = 0;
+	for (int v : i) {
+		sum += v;
 	}
-//
-//// To pick up function references (that will become function pointers)
-//template<typename _Functor, typename ... _Args>
-//inline typename std::enable_if<
-//(std::is_pointer<_Functor>::value
-//		&& std::is_function<typename std::remove_pointer<_Functor>::type>::value),
-//		typename std::result_of<_Functor(_Args&&...)>::type>::type __invoke_(
-//				_Functor __f, _Args&&... __args) {
-//	std::cout << "Function" << std::endl;
-//	return __f(std::forward<_Args>(__args)...);
-//}
-
-	int repeat(std::vector<int>&& i) {
-		int sum = 0;
-		for (int v : i) {
-			sum += v;
-		}
-		i.clear();
-		return sum;
-	}
-
-//	int repeat(std::vector<int> i) {
-//		int sum = 0;
-//		for (int v : i) {
-//			sum += v;
-//		}
-//		i.clear();
-//		return sum;
-//	}
-
-	class crr {
-		int v;
-	public:
-		crr(int v) :
-				v(v) {
-		}
-
-		int blah(std::vector<int>&& l) {
-			int sum = v;
-			for (int v : l) {
-				sum -= v;
-			}
-			return sum;
-		}
-
-		int operator()(std::vector<int>&& l) {
-			return blah(std::move(l));
-		}
-	};
-
-	class brr {
-	public:
-		int v;
-		crr c;
-		std::vector<int> b;
-		brr(int k) :
-				v(k), c(k) {
-		}
-
-		int blah(std::vector<int>&& l) {
-			int sum = v;
-			for (int v : l) {
-				sum += v;
-			}
-			b = std::move(l);
-			return sum;
-		}
-
-		int operator()(std::vector<int>&& l) {
-			return blah(std::move(l));
-		}
-	};
-
-//template <class T>
-//struct is_member_pointer
-//{
-//  template <class X, class Y>
-//  static char is_ptr(Y X::*);
-//  static double is_ptr(...);
-//
-//  static T t;
-//  enum { value = sizeof(is_ptr(t)) == sizeof(char) };
-//};
-
-template<typename X, typename Y>
-bool is_member_pointer(Y X::*) {
-	return true;
+	return sum;
 }
 
-template<typename X>
-bool is_member_pointer(X x) {
-	return false;
-}
-
-int fa(int b) {
+int overloaded_function(int b) {
 	return b;
 }
 
-int fa(int b, int c) {
-	return b+c;
+int overloaded_function(int b, int c) {
+	return b + c;
 }
 
-int doWrapTest() {
+class class_c {
+	int v;
+public:
+	class_c(int v) :
+			v(v) {
+	}
+
+	int member_function_move(std::vector<int>&& l) {
+		int sum = v;
+		for (int v : l) {
+			sum -= v;
+		}
+		return sum;
+	}
+
+	static int static_function(int v) {
+		return v;
+	}
+
+	int operator()(std::vector<int>&& l) {
+		return member_function_move(std::move(l));
+	}
+};
+
+class class_b {
+public:
+	int member_object;
+	class_c c;
+	std::vector<int> b;
+	class_b(int k) :
+			member_object(k), c(k) {
+	}
+
+	int member_function_move(std::vector<int>&& l) {
+		int sum = member_object;
+		for (int v : l) {
+			sum += v;
+		}
+		b = std::move(l);
+		return sum;
+	}
+
+	int operator()(std::vector<int>&& l) {
+		return member_function_move(std::move(l));
+	}
+};
+
+int doInvokeTest() {
 
 	int i = 10;
-	std::vector<int> v {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	brr b {2};
-	std::unique_ptr<brr> bp(new brr {5});
+	std::vector<int> v { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	class_b b { 2 };
+	std::unique_ptr<class_b> bp(new class_b { 5 });
 
-	// auto f = function_wrapper<std::function<int(std::unique_ptr<int>&&)>, std::unique_ptr<int>&&>(repeat, std::move(pnt));
-	// auto f = function_wrapper<std::function<int(std::unique_ptr<int>&&)>, std::unique_ptr<int>&&>(repeat, std::move(pnt));
-	// auto f = function_wrapper<decltype(&repeat),std::vector<int>&&>(repeat, std::move(v));
-	// auto f = wrap(&repeat, std::move(v));
-	// auto f = wrap(std::move(b), std::move(v));
-	// auto f = wrap(&brr::blah,&b, std::move(v));
-	// auto f = function_wrapper<decltype(&brr::blah),brr*,std::vector<int>&&>(&brr::blah, &b, std::move(v));
-	// auto f = wrap(&brr::blah, std::move(b), std::move(v));
+	using mas::concurrency::invoke;
 
 	int outa, outb, outc, outd, oute;
-	outa = invoke<int(&)(int,int)>(fa, 1, 16);
-	outa = invoke<int(int,int)>(fa, 1, 16);
-	outa = invoke<int(int)>(fa, 1);
-	outa = invoke(&repeat, std::move(v));
+	outa = invoke<int (&)(int, int)>(overloaded_function, 1, 16);
+	outa = invoke<int(int, int)>(overloaded_function, 1, 16);
+	outa = invoke<int(int)>(overloaded_function, 1);
+	outa = invoke(&regular_function, std::move(v));
 
 	outb = invoke(b, std::move(v));
 	outb = invoke(std::ref(b), std::move(v));
-	outc = invoke(std::move(b), std::move(v));
+	outb = invoke(std::move(b), std::move(v));
 
-	outd = invoke(&brr::blah, b, std::move(v));
-	outd = invoke(&brr::blah, &b, std::move(v));
-	outd = invoke(&brr::blah, std::move(b), std::move(v));
-	outd = invoke(&brr::blah, bp, std::move(v));
-	oute = invoke(&brr::v, b);
-	oute = invoke(&brr::v, &b);
+	outc = invoke(&class_c::static_function,17);
 
-	//__test_(&brr::blah, b, std::move(v));
-	//bool t = std::is_member_function_pointer<decltype(&brr::blah)>::value;
-	//std::cout << t << std::endl;
+	outd = invoke(&class_b::member_function_move, b, std::move(v));
+	outd = invoke(&class_b::member_function_move, &b, std::move(v));
+	outd = invoke(&class_b::member_function_move, std::move(b), std::move(v));
+	outd = invoke(&class_b::member_function_move, bp, std::move(v));
+
+	oute = invoke(&class_b::member_object, b);
+	oute = invoke(&class_b::member_object, &b);
 
 	std::cout << outa << std::endl;
 	std::cout << outb << std::endl;
 	std::cout << outc << std::endl;
 	std::cout << outd << std::endl;
-
+	std::cout << oute << std::endl;
 
 	return 0;
 
 }
 
+void doWrapTest() {
+	// auto f = function_wrapper<std::function<int(std::unique_ptr<int>&&)>, std::unique_ptr<int>&&>(regular_function, std::move(pnt));
+	// auto f = function_wrapper<std::function<int(std::unique_ptr<int>&&)>, std::unique_ptr<int>&&>(repeat, std::move(pnt));
+	// auto f = function_wrapper<decltype(&regular_function),std::vector<int>&&>(regular_function, std::move(member_object));
+	// auto f = wrap(&regular_function, std::move(member_object));
+	// auto f = wrap(std::move(b), std::move(member_object));
+	// auto f = wrap(&class_b::member_function_move,&b, std::move(member_object));
+	// auto f = function_wrapper<decltype(&class_b::member_function_move),class_b*,std::vector<int>&&>(&class_b::member_function_move, &b, std::move(member_object));
+	// auto f = wrap(&brr::member_function_move, std::move(b), std::move(member_object));
+
+	int i = 10;
+	std::vector<int> v { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	class_b b { 2 };
+	std::unique_ptr<class_b> bp(new class_b { 5 });
+
+	using namespace mas::concurrency;
+
+	// regular
+	//auto fa = wrap<int(int, int)>(overloaded_function, 1, 16);
+	auto fa = bind_simple<int(int,int)>(overloaded_function, 1, 16);
+	//auto outa = fa();
+	//std::cout << outa << std::endl;
+}
+
 int main(int argc, char **argv) {
 	doWrapTest();
+	//doInvokeTest();
 }
 
