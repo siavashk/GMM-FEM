cc403d006812142cd55f7d145dbf9227dd974bc7
Antonio Sánchez <antonio@panthera.ca>
2015-01-11 22:46:56 -0800

    wrapped functions now working

--- test/mas/concurrency/tuple_test.cpp
+++ test/mas/concurrency/tuple_test.cpp
@@ -21,6 +21,19 @@ int overloaded_function(int b, int c) {
 	return b + c;
 }
 
+class class_a {
+public:
+	int a;
+	class_a() : a(0) {}
+	int operator()(int v) {
+		a = a+v;
+		return a;
+	}
+	void add(int v) {
+		a += v;
+	}
+};
+
 class class_c {
 	int v;
 public:
@@ -97,11 +110,15 @@ int doInvokeTest() {
 	oute = invoke(&class_b::member_object, b);
 	oute = invoke(&class_b::member_object, &b);
 
-	std::cout << outa << std::endl;
-	std::cout << outb << std::endl;
-	std::cout << outc << std::endl;
-	std::cout << outd << std::endl;
-	std::cout << oute << std::endl;
+	class_a a;
+	invoke(&class_a::add, a, 10);
+	std::cout << a.a << std::endl;
+
+	//	std::cout << outa << std::endl;
+	//	std::cout << outb << std::endl;
+	//	std::cout << outc << std::endl;
+	//	std::cout << outd << std::endl;
+	//	std::cout << oute << std::endl;
 
 	return 0;
 
@@ -125,14 +142,59 @@ void doWrapTest() {
 	using namespace mas::concurrency;
 
 	// regular
-	//auto fa = wrap<int(int, int)>(overloaded_function, 1, 16);
-	auto fa = bind_simple<int(int,int)>(overloaded_function, 1, 16);
-	//auto outa = fa();
-	//std::cout << outa << std::endl;
+	auto f1 = wrap<int(&)(int, int)>(overloaded_function, 1, 16); f1();
+	auto f2 = wrap<int(int, int)>(overloaded_function, 1, 16); f2();
+	auto f3 = wrap<int(int)>(overloaded_function, 1); f3();
+	auto f4 = wrap(&regular_function, std::move(v)); f4();
+
+	auto f5 = wrap(b, std::move(v)); f5();
+	auto f6 = wrap(std::ref(b), std::move(v)); f6();
+	auto f7 = wrap(std::move(b), std::move(v)); f7();
+
+	auto f8 = wrap(&class_c::static_function,17); f8();
+
+	print_type_info(&class_b::member_function_move, b, std::move(v));
+	std::cout << std::endl;
+	print_type_info(&class_b::member_function_move, &b, std::move(v));
+	std::cout << std::endl;
+	print_type_info(&class_b::member_function_move, std::move(b), std::move(v));
+	std::cout << std::endl;
+	print_type_info(&class_b::member_function_move, std::move(bp), std::move(v));
+
+//	auto f9  = wrap(&class_b::member_function_move, b, std::move(v)); f9();
+	auto f10 = wrap(&class_b::member_function_move, &b, std::move(v)); f10();
+//	auto f11 = wrap(&class_b::member_function_move, std::move(b), std::move(v)); f11();
+	auto f12 = wrap(&class_b::member_function_move, std::move(bp), std::move(v)); f12();
+
+	class_a a;
+	auto f13 = wrap(&class_a::add, &a, 10); f13();
+	std::cout << a.a << std::endl;
+
+//	auto f14 = wrap(&class_b::member_object, &b); f14();
+	auto f15 = wrap(&class_b::member_object, &b); f15();
+
+}
+
+void doScopeTest() {
+
+	using namespace mas::concurrency;
+	std::unique_ptr<function_wrapper_t<int(std::vector<int>&&),std::vector<int>&&>::type> func;
+	{
+		std::vector<int> v { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		func = std::move(make_unique_wrapper(regular_function, std::move(v)));
+		v.clear();
+		v.shrink_to_fit();
+	}
+
+	// v should no longer exist, but will the function run?
+	int out = func->invoke();
+	std::cout << out << std::endl;
+
 }
 
 int main(int argc, char **argv) {
-	doWrapTest();
-	//doInvokeTest();
+	// doWrapTest();
+	// doInvokeTest();
+	doScopeTest();
 }
 
